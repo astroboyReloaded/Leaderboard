d52d4503f76d4878d0e7fa6527512ff5a9cf45b5
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-03-15 16:35:32 -0600

    Create renderScores() async to GET request

--- src/modules/renderScores.js
+++ src/modules/renderScores.js
@@ -1,13 +1,17 @@
-import Scores from '../../temporaryDb.js';
-
 const scoresDiv = document.getElementById('scores');
 
-const renderScores = () => {
-  scoresDiv.innerHTML = `<ul class="scoresUl">
+const renderScores = async () => {
+  await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/JW2D0NchqUvcYPBtOfmC/scores/'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const Scores = data.result;
+      scoresDiv.innerHTML = `<ul class="scoresUl">
     ${Scores.map(
-    (score) => `<li class="scoreLi">${score.name}: ${score.score}</li>`,
-  ).join('')}
+      (score) => `<li class="scoreLi">${score.user}: ${score.score}</li>`
+    ).join('')}
   </ul>`;
+    });
 };
-
 export default renderScores;
