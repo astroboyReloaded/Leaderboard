d6abaff54a6e5299537cc9f43ab56ba355075b06
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-03-15 16:33:43 -0600

    Create submitScores() async to GET request

--- src/modules/submitScore.js
+++ src/modules/submitScore.js
@@ -1,5 +1,17 @@
-const submitScore = (e) => {
+const [name, score] = document.getElementsByTagName('input');
+
+const submitScore = async (e) => {
   e.preventDefault();
+  await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/JW2D0NchqUvcYPBtOfmC/scores/',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user: name.value, score: score.value }),
+    }
+  );
 };
 
 export default submitScore;
